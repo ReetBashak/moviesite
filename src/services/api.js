f13305f56187920178edb6c01f0d250ac1eb27ae
ReetBashak <ritikabashak55@gmail.com>
2025-09-05 12:44:45 +0530

    Add

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "a1574042a5a15cb7ac7cb2d86b209c31";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
