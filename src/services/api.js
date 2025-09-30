16de4ed80bd0d21f9304de465c2e615f4930b870
RitikaBashak <ritikabashak44@gmail.com>
2025-09-30 14:03:04 +0530

    ignore

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
-const API_KEY = "a1574042a5a15cb7ac7cb2d86b209c31";
-const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}`
+  );
   const data = await response.json();
   return data.results;
 };
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+    `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(
       query
     )}`
   );
